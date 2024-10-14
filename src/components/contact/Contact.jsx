cabd5b09dede7066118de19a02e80327a2bff0ac
Tarun Durbha <tarundurbha14@gmail.com>
2024-10-14 08:17:03 +0530

    made changes in contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -58,15 +58,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>tarundurbha14@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>KLM Street ,RJY,INDIA</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>6301290869</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
